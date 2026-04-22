d4e3969b7a86968edbd0b0dc72b395c7dda70851
Iroshanapragith120 <149234803+Iroshanapragith120@users.noreply.github.com>
2026-04-22 15:18:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    OWNER_NUMBER: "94741433513", // මෙතනට උඹේ නම්බර් එක විතරක් දාපන් (Prefix නැතුව)
+    OWNER_NUMBER: "94741433513", 
     PREFIX: ".",
-    SESSION_DATA: "./setting"
+    WORK_MODE: "private" // default private තියෙන්නේ
 };
