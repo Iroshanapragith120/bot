23aa172abdc62bce7ee0f5de07903dad463bba50
Iroshanapragith120 <149234803+Iroshanapragith120@users.noreply.github.com>
2026-04-23 10:20:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     OWNER_NUMBER: "94741433513", 
     PREFIX: ".",
-    WORK_MODE: "private" // default private තියෙන්නේ
+    WORK_MODE: "private",
+    SESSION_ID: "ඔයාගේ_සෙෂන්_අයිඩී_එක_මෙතනට_දාන්න" // උදා: GEMINI;;;xyz...
 };
